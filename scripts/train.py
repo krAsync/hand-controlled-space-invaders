c44fdba60669cc0d6285485ef7987830318728c6
krAsync <ondrej.brdek@gmail.com>
2026-01-30 17:54:30 +0100

    better model paths

--- scripts/train.py
+++ scripts/train.py
@@ -4,19 +4,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.preprocessing import LabelEncoder
 import joblib
+from pathlib import Path 
 
-df = pd.read_csv('/home/kr3915/Projects/MediaControl/data/retro/gestures.csv', header=None)
-
+script_path = Path(__file__).resolve()
+root_dir = script_path.parent.parent
+data_path = root_dir/'data'/'retro'/'gestures.csv'
+model_path = root_dir/'models'/'rf_model.pkl'
+df = pd.read_csv(data_path, header=None)
 if len(df) < 2:
     print("Not enough data to train the model. Please capture more gestures.")
 else:
-    y = df.iloc[:, 0]
+    y = df.iloc[1:, 0]
 
     # Encode handedness (column 1)
-    handedness = df.iloc[:, 1].astype('category').cat.codes
+    handedness = df.iloc[1:, 1].astype('category').cat.codes
 
     # Landmark data starts from column 2
-    landmarks = df.iloc[:, 2:]
+    landmarks = df.iloc[1:, 2:]
 
     # Combine handedness and landmarks into features
     X = pd.concat([handedness, landmarks], axis=1)
@@ -30,5 +34,5 @@ else:
 
     accuracy = model.score(X_test, y_test)
     print(f'Accuracy for unified model: {accuracy:.2f}')
-    joblib.dump(model, '/home/kr3915/Projects/MediaControl/models/gesture_model.pkl')
+    joblib.dump(model, model_path)
     print('Saved unified gesture model.')
